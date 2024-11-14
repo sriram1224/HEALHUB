d0e350cc058b1b18e67de0395123992c963d3535
Bhargav <kasukurthibhargav@gmail.com>
2024-11-14 16:08:10 +0530

    change port

---
+++ .history/frontend/src/context/AppContext_20241114154241.jsx
@@ -0,0 +1,18 @@
+import { createContext } from "react";
+import PropTypes from "prop-types";
+import { doctors } from "../assets/assets";
+
+export const AppContext = createContext();
+
+const AppContextProvider = (props) => {
+  const value = {
+    doctors,
+  };
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
+AppContextProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+export default AppContextProvider;
